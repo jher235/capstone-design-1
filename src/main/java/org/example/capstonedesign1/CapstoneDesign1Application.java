9fc5fa51c72bfba646bf6ddef34622a2b711a48b
jher235 <tim668666@gmail.com>
2025-03-15 22:48:38 +0900

    setting: 프로젝트 세팅 - #2

---
+++ src/main/java/org/example/capstonedesign1/CapstoneDesign1Application.java
@@ -0,0 +1,13 @@
+package org.example.capstonedesign1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneDesign1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneDesign1Application.class, args);
+    }
+
+}
