fe0b5f07e80fa38adeba234baf56953704864da6
jher235 <tim668666@gmail.com>
2025-03-17 23:47:38 +0900

    feat: responseDto로 응답 통일  - #4

---
+++ src/main/java/org/example/capstonedesign1/global/dto/ResponseDto.java
@@ -0,0 +1,33 @@
+package org.example.capstonedesign1.global.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@JsonPropertyOrder({"code", "message", "timestamp", "data"})
+public class ResponseDto<T> {
+
+    private final String statusCode;
+    private final String message;
+    private final LocalDateTime timeStamp;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private final T data;
+
+
+    public static <T> ResponseDto<T> res(HttpStatus status, String message){
+        return new ResponseDto<>(String.valueOf(status), message, LocalDateTime.now(), null);
+    }
+
+    public static <T> ResponseDto<T> res(HttpStatus status, String message, T data){
+        return new ResponseDto<>(String.valueOf(status), message, LocalDateTime.now(), data);
+    }
+}
