bfb45406b911591a5162e5940da041b9fbc3c516
jher235 <tim668666@gmail.com>
2025-03-19 15:01:23 +0900

    feat: 로그인 필터, jwt 토큰 인증 필터 등록 - #4

--- src/main/java/org/example/capstonedesign1/global/config/SecurityConfig.java
+++ src/main/java/org/example/capstonedesign1/global/config/SecurityConfig.java
@@ -1,18 +1,33 @@
 package org.example.capstonedesign1.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.example.capstonedesign1.domain.auth.jwt.JwtProvider;
+import org.example.capstonedesign1.domain.auth.security.CustomAuthenticationEntryPoint;
+import org.example.capstonedesign1.domain.auth.security.filter.JwtAuthenticationFilter;
+import org.example.capstonedesign1.domain.auth.security.filter.JwtLoginFilter;
+import org.example.capstonedesign1.domain.auth.service.TokenService;
+import org.example.capstonedesign1.domain.user.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final TokenService tokenService;
+    private final ObjectMapper objectMapper;
+    private final JwtProvider jwtProvider;
+    private final UserService userService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -28,11 +43,44 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 ;
 
+        http
+                .addFilterAt(getJwtLoginFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(exceptions ->
+                        exceptions.authenticationEntryPoint(customAuthenticationEntryPoint()))
+                ;
+
+
         return http.build();
+    }
+
+    @Bean
+    public CustomAuthenticationEntryPoint customAuthenticationEntryPoint(){
+        return new CustomAuthenticationEntryPoint(objectMapper);
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(){
+        return new JwtAuthenticationFilter(jwtProvider, userService, objectMapper);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    private JwtLoginFilter getJwtLoginFilter() throws Exception {
+
+        JwtLoginFilter jwtLoginFilter = new JwtLoginFilter(
+                authenticationManager(authenticationConfiguration),
+                tokenService,
+                objectMapper);
+        jwtLoginFilter.setFilterProcessesUrl("/auth/login");
+        return jwtLoginFilter;
     }
 }
