fdae26a0202b5df829be965d3a57aa199675fbd7
jher235 <tim668666@gmail.com>
2025-03-19 14:44:55 +0900

    feat: 회원가입과 로그인 시 반환되는 AuthenticationResponse dto 작성 - #4

--- src/main/java/org/example/capstonedesign1/domain/auth/service/AuthService.java
+++ src/main/java/org/example/capstonedesign1/domain/auth/service/AuthService.java
@@ -1,20 +1,17 @@
 package org.example.capstonedesign1.domain.auth.service;
 
 import lombok.RequiredArgsConstructor;
-import org.antlr.v4.runtime.Token;
 import org.example.capstonedesign1.domain.auth.dto.request.SignUpCompleteRequest;
 import org.example.capstonedesign1.domain.auth.dto.request.SignUpRequest;
-import org.example.capstonedesign1.domain.auth.dto.response.SignUpResponse;
-import org.example.capstonedesign1.domain.auth.jwt.JwtUtil;
+import org.example.capstonedesign1.domain.auth.dto.response.AuthenticationResponse;
 import org.example.capstonedesign1.domain.user.entity.Profile;
 import org.example.capstonedesign1.domain.user.entity.User;
-import org.example.capstonedesign1.domain.user.entity.enums.Status;
 import org.example.capstonedesign1.domain.user.repository.UserRepository;
 import org.example.capstonedesign1.global.exception.ConflictException;
 import org.example.capstonedesign1.global.exception.code.ErrorCode;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -29,7 +26,7 @@ public class AuthService {
      * id, pw 회원가입
      * @param request
      */
-    public SignUpResponse signUp(SignUpRequest request){
+    public AuthenticationResponse signUp(SignUpRequest request){
         if(userRepository.existsByEmail(request.getEmail())){
             throw new ConflictException(ErrorCode.CONFLICT_EMAIL);
         }
@@ -37,7 +34,7 @@ public class AuthService {
         User user = new User(request.getEmail(), passwordEncoder.encode(request.getPassword()));
         userRepository.save(user);
         String accessToken = tokenService.issueAccessToken(user.getId().toString());
-        return new SignUpResponse(accessToken, user.isRegisterCompleted());
+        return new AuthenticationResponse(accessToken, user.isRegisterCompleted());
     }
 
     /**
