fdbbc42ff9e793cb4f5b03f2e3068e78d767f802
jher235 <tim668666@gmail.com>
2025-03-19 14:59:10 +0900

    feat: TokenService 작성 - #4

--- src/main/java/org/example/capstonedesign1/domain/auth/service/TokenService.java
+++ src/main/java/org/example/capstonedesign1/domain/auth/service/TokenService.java
@@ -1,16 +1,15 @@
 package org.example.capstonedesign1.domain.auth.service;
 
 import lombok.RequiredArgsConstructor;
-import org.example.capstonedesign1.domain.auth.jwt.JwtUtil;
+import org.example.capstonedesign1.domain.auth.jwt.JwtProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class TokenService {
-    private final JwtUtil jwtUtil;
-
+    private final JwtProvider jwtProvider;
     public String issueAccessToken(String subject){
-        return jwtUtil.issueAccessToken(subject);
+        return jwtProvider.issueAccessToken(subject);
     }
 
 }
