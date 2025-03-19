8759b57ed9573445cd3ae732277a09aac8f3fbdb
jher235 <tim668666@gmail.com>
2025-03-19 14:54:32 +0900

    feat: 인증 필터 단에서 발생하는 예외처리 클래스 작성 - #4 AuthenticationEntryPoint을 상속 받아 사용함

---
+++ src/main/java/org/example/capstonedesign1/domain/auth/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,33 @@
+package org.example.capstonedesign1.domain.auth.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.example.capstonedesign1.global.dto.ErrorResponseDto;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+
+        response.getWriter().write(objectMapper.writeValueAsString(
+                ErrorResponseDto.res(String.valueOf(HttpServletResponse.SC_UNAUTHORIZED), authException)));
+    }
+
+}
