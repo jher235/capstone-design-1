7df1de706d8eda74b17f4738efdaf6e3e09f717e
jher235 <tim668666@gmail.com>
2025-03-24 19:00:54 +0900

    feat: 금융 성향 분석 컨트롤러단 작성 - #6

--- src/main/java/org/example/capstonedesign1/domain/propensity/controller/PropensityController.java
+++ src/main/java/org/example/capstonedesign1/domain/propensity/controller/PropensityController.java
@@ -5,13 +5,16 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.example.capstonedesign1.domain.auth.dto.request.LoginRequest;
-import org.example.capstonedesign1.domain.auth.dto.response.AuthenticationResponse;
+import org.example.capstonedesign1.domain.auth.security.CustomUserDetails;
+import org.example.capstonedesign1.domain.propensity.dto.request.SurveyRequest;
+import org.example.capstonedesign1.domain.propensity.dto.response.PropensityAnalysisResponse;
 import org.example.capstonedesign1.domain.propensity.dto.response.SurveyResponse;
+import org.example.capstonedesign1.domain.propensity.service.PropensityCommandService;
 import org.example.capstonedesign1.domain.propensity.service.PropensityQueryService;
 import org.example.capstonedesign1.global.dto.ResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/propensity")
 public class PropensityController {
     private final PropensityQueryService propensityQueryService;
+    private final PropensityCommandService propensityCommandService;
 
     @Operation(
             summary = "금융 성향 설문 문항 조회 API",
@@ -34,5 +38,20 @@ public class PropensityController {
         return new ResponseEntity<>(ResponseDto.res(HttpStatus.OK, "문항 조회 성공", surveyResponse), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "금융 성향 설문 제출 API",
+            description = "금융 성향을 판단하기 위한 응답 요청하는 api"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "금융 성향 분석 성공"),
+            @ApiResponse(responseCode = "200", description = "금융 성향 분석 성공")
+    })
+    @PostMapping
+    public ResponseEntity<ResponseDto<PropensityAnalysisResponse>> postSurvey(@Valid @RequestBody SurveyRequest request,
+                                                                              @AuthenticationPrincipal CustomUserDetails userDetails){
+        PropensityAnalysisResponse response =  propensityCommandService.submitSurvey(userDetails.getUser(), request);
+
+        return new ResponseEntity<>(ResponseDto.res(HttpStatus.OK, "금융 성향 분석 성공", response), HttpStatus.OK);
+    }
 
 }
