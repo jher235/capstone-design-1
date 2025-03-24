25f866f017bd692e96c311445a5cd4385ed74c88
jher235 <tim668666@gmail.com>
2025-03-24 18:56:32 +0900

    feat: 금융 성향 분석 결과 dto 작성 - #6

---
+++ src/main/java/org/example/capstonedesign1/domain/propensity/dto/response/PropensityAnalysisResponse.java
@@ -0,0 +1,9 @@
+package org.example.capstonedesign1.domain.propensity.dto.response;
+
+
+
+public record PropensityAnalysisResponse(String type,
+                                         String description,
+                                         String prosAndCons,
+                                         String precaution) {
+}
