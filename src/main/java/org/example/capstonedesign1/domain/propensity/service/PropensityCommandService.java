64a638a7492618c024e96ee33b1b8ece9d8be979
jher235 <tim668666@gmail.com>
2025-03-24 18:58:39 +0900

    feat: 금융 성향 설문 제출 - #6

---
+++ src/main/java/org/example/capstonedesign1/domain/propensity/service/PropensityCommandService.java
@@ -0,0 +1,46 @@
+package org.example.capstonedesign1.domain.propensity.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.capstonedesign1.domain.chat.client.OpenAiApiClient;
+import org.example.capstonedesign1.domain.chat.dto.Message;
+import org.example.capstonedesign1.domain.chat.dto.response.OpenAiResponse;
+import org.example.capstonedesign1.domain.propensity.dto.request.SurveyRequest;
+import org.example.capstonedesign1.domain.propensity.dto.response.PropensityAnalysisResponse;
+import org.example.capstonedesign1.domain.propensity.entity.UserPropensity;
+import org.example.capstonedesign1.domain.propensity.repository.UserPropensityRepository;
+import org.example.capstonedesign1.domain.user.entity.User;
+import org.example.capstonedesign1.global.template.PromptTemplate;
+import org.example.capstonedesign1.global.util.JsonUtil;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static org.example.capstonedesign1.domain.chat.client.OpenAiApiClient.SYSTEM_ROLE;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PropensityCommandService {
+
+    private final OpenAiApiClient openAiApiClient;
+
+    private final UserPropensityRepository userPropensityRepository;
+
+    public PropensityAnalysisResponse submitSurvey(User user, SurveyRequest request){
+        String requestMessage =  PromptTemplate.propensityAnalysisPrompt(
+                user.getProfile().getGender().toString(),
+                request.getSurveyEntries());
+
+        OpenAiResponse response =  openAiApiClient.sendRequest(List.of(new Message(SYSTEM_ROLE, requestMessage)) );
+
+        String content = response.getChoices().get(0).getMessage().getContent().trim();
+
+        PropensityAnalysisResponse propensityAnalysisResponse = JsonUtil.parseClass(
+                PropensityAnalysisResponse.class, content);
+
+        userPropensityRepository.save(new UserPropensity(user, propensityAnalysisResponse.type(), content));
+        return propensityAnalysisResponse;
+    }
+
+}
