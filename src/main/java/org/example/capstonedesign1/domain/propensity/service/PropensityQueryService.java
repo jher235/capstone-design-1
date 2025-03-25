d85b0e8b0939c335462d42149c35ad8684fd7b8c
jher235 <tim668666@gmail.com>
2025-03-25 10:19:42 +0900

    feat: userPropensity 찾는 메서드 추가 - #6

--- src/main/java/org/example/capstonedesign1/domain/propensity/service/PropensityQueryService.java
+++ src/main/java/org/example/capstonedesign1/domain/propensity/service/PropensityQueryService.java
@@ -4,10 +4,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.capstonedesign1.domain.propensity.dto.response.SurveyResponse;
 import org.example.capstonedesign1.domain.propensity.entity.PropensityQuestion;
+import org.example.capstonedesign1.domain.propensity.entity.UserPropensity;
 import org.example.capstonedesign1.domain.propensity.repository.PropensityQuestionRepository;
+import org.example.capstonedesign1.domain.propensity.repository.UserPropensityRepository;
+import org.example.capstonedesign1.global.exception.NotFoundException;
+import org.example.capstonedesign1.global.exception.code.ErrorCode;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Slf4j
 @Service
@@ -15,10 +20,16 @@ import java.util.List;
 public class PropensityQueryService {
 
     private final PropensityQuestionRepository propensityQuestionRepository;
+    private final UserPropensityRepository userPropensityRepository;
 
     public SurveyResponse getSurvey(){
         List<PropensityQuestion> questions = propensityQuestionRepository.findAllWithOptions();
         return SurveyResponse.from(questions);
+    }
+
+    public UserPropensity findUserPropensityById(UUID userPropensityId){
+        return userPropensityRepository.findById(userPropensityId).orElseThrow(()
+                -> new NotFoundException(ErrorCode.USER_PROPENSITY_NOT_FOUND));
     }
 
 }
