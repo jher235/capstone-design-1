988516c37b1354526a8884c58632110053f8ffa2
jher235 <tim668666@gmail.com>
2025-03-25 23:26:11 +0900

    feat: projection을 사용하여 필요한 부분만 가져오도록 함 - #9

---
+++ src/main/java/org/example/capstonedesign1/domain/propensity/dto/response/projection/UserPropensityPreview.java
@@ -0,0 +1,8 @@
+package org.example.capstonedesign1.domain.propensity.dto.response.projection;
+
+import java.util.UUID;
+
+public interface UserPropensityPreview {
+    UUID getId();
+    String getPropensity();
+}
