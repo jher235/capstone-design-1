5daccb9e840a388ea4c76e65ee5d37a3aa4e1f94
jher235 <tim668666@gmail.com>
2025-03-28 11:47:35 +0900

    feat: 금융 상품 추천 결과를 저장할 BankProductRecommendation 작성 - #8

---
+++ src/main/java/org/example/capstonedesign1/domain/bankproduct/entity/BankProductRecommendation.java
@@ -0,0 +1,24 @@
+package org.example.capstonedesign1.domain.bankproduct.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.capstonedesign1.domain.user.entity.User;
+import org.example.capstonedesign1.global.common.BaseEntity;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BankProductRecommendation extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    User user;
+
+    @Column(nullable = false, columnDefinition = "JSON")
+    String content;
+
+}
