10f5c504154d5ac33c862d8cd83f895b9376db0b
jher235 <tim668666@gmail.com>
2025-04-04 12:13:51 +0900

    feat: 카드 추천 결과 엔티티 작성 - #12 정규화를 고려중... json을 사용하면 column이 들어가서 용량이 커지는 단점이 있고, 정규화를 하면 이후에 금융 추천과 카드 추천을 한번에 조회할 수 없다는 점, 추천 결과에 상품이 1개 이상 존재할 수 있으므로 추천 결과와 금융 상품 사이에 액션 테이블이 추가로 생길 수 있다는 점이 걸림.

---
+++ src/main/java/org/example/capstonedesign1/domain/cardproduct/entity/CardProductRecommendation.java
@@ -0,0 +1,22 @@
+package org.example.capstonedesign1.domain.cardproduct.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.capstonedesign1.domain.user.entity.User;
+import org.example.capstonedesign1.global.common.BaseEntity;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CardProductRecommendation extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    User user;
+
+    @Column(nullable = false, columnDefinition = "JSON")
+    String content;
+}
