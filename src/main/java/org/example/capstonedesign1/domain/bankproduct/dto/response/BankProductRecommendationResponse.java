2f9ed5f5ab9587e32770f2ede564e44f2fb94b09
jher235 <tim668666@gmail.com>
2025-04-22 09:58:44 +0900

    feat: 정적 팩토리 메서드 추가 - #15

--- src/main/java/org/example/capstonedesign1/domain/bankproduct/dto/response/BankProductRecommendationResponse.java
+++ src/main/java/org/example/capstonedesign1/domain/bankproduct/dto/response/BankProductRecommendationResponse.java
@@ -1,14 +1,18 @@
 package org.example.capstonedesign1.domain.bankproduct.dto.response;
 
 
-import org.example.capstonedesign1.domain.bankproduct.dto.json.BankProductRecommendationContent;
+import org.example.capstonedesign1.domain.bankproduct.entity.BankProductRecommendation;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record BankProductRecommendationResponse (UUID id,
-                                                 BankProductRecommendationContent content,
-                                                 LocalDateTime createdAt
-                                                 ){
-
+public record BankProductRecommendationResponse(UUID id,
+                                                String strategy,
+                                                String content,
+                                                LocalDateTime createdAt
+) {
+    public static BankProductRecommendationResponse from(BankProductRecommendation recommendation) {
+        return new BankProductRecommendationResponse(
+                recommendation.getId(), recommendation.getStrategy(), recommendation.getContent(), recommendation.getCreatedAt());
+    }
 }
