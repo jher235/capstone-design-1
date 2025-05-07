e319122a6738b33861c1556bf0309bb807ba7259
jher235 <tim668666@gmail.com>
2025-05-07 14:37:14 +0900

    fix: 실수로 전부 주석 처리되었던 @RestControllerAdvice 복구 - #18

--- src/main/java/org/example/capstonedesign1/global/exception/controller/ExceptionController.java
+++ src/main/java/org/example/capstonedesign1/global/exception/controller/ExceptionController.java
@@ -12,8 +12,10 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-//@RestControllerAdvice(annotations = {RestController.class},
+@RestControllerAdvice
+//        (annotations = {RestController.class},
 //        basePackageClasses = {
 //                AdminController.class,
 //                AuthController.class,
