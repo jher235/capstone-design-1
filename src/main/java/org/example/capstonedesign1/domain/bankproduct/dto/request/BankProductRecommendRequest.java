e8c78bf9b887c8899cb5e9f1eacb7279df7f274f
jher235 <tim668666@gmail.com>
2025-05-11 00:01:13 +0900

    feat: 금융성향 필드 추가 - #20

--- src/main/java/org/example/capstonedesign1/domain/bankproduct/dto/request/BankProductRecommendRequest.java
+++ src/main/java/org/example/capstonedesign1/domain/bankproduct/dto/request/BankProductRecommendRequest.java
@@ -1,9 +1,16 @@
 package org.example.capstonedesign1.domain.bankproduct.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.capstonedesign1.domain.propensity.entity.enums.Propensity;
+
+import static org.example.capstonedesign1.domain.bankproduct.constant.BankProductConstant.MAX_INVEST_AMOUNT;
+import static org.example.capstonedesign1.domain.bankproduct.constant.BankProductConstant.MIN_INVEST_AMOUNT;
 
 @Getter
 @NoArgsConstructor
@@ -22,5 +29,18 @@ public class BankProductRecommendRequest {
     @Min(value = 0, message = "투자 금액은 0원 이상이어야 합니다.")
     @Max(value = 10_000_000_000_000L, message = "투자 금액은 1경 원을 넘을 수 없습니다.")
     private Long amount;
+
+    @Schema(description = """
+            투자 성향
+            CONSERVATIVE: 보수형,
+            CONSUMER: 소비형,
+            INVESTOR: 투자형,
+            BALANCED: 균형형,
+            FLEXIBLE: 융합형
+            """,
+            example = "CONSERVATIVE",
+            allowableValues = {"CONSERVATIVE", "CONSUMER", "INVESTOR", "BALANCED", "FLEXIBLE"})
+    @NotNull(message = "투자 성향은 필수입니다.")
+    private Propensity propensity;
 
 }
