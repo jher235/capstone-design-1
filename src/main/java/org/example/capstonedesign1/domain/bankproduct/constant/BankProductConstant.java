389eb767c6e44af06c39be2ec9c639ba5bf8731e
jher235 <tim668666@gmail.com>
2025-05-11 13:55:08 +0900

    fix: 타입 수정 - #20 @Max, @Min 에서 value로 기대하는 long 타입으로 수정

--- src/main/java/org/example/capstonedesign1/domain/bankproduct/constant/BankProductConstant.java
+++ src/main/java/org/example/capstonedesign1/domain/bankproduct/constant/BankProductConstant.java
@@ -6,6 +6,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class BankProductConstant {
 
-    public static final Long MIN_INVEST_AMOUNT = 0L;
-    public static final Long MAX_INVEST_AMOUNT = 10_000_000_000_000L;
+    public static final long MIN_INVEST_AMOUNT = 0L;
+    public static final long MAX_INVEST_AMOUNT = 10_000_000_000_000L;
 }
