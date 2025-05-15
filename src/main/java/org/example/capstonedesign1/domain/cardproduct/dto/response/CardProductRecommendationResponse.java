78ffd32aca24c314bbbaa582d94299a82ccb4693
jher235 <tim668666@gmail.com>
2025-05-16 00:28:56 +0900

    style: 코드 포맷팅 - #26

--- src/main/java/org/example/capstonedesign1/domain/cardproduct/dto/response/CardProductRecommendationResponse.java
+++ src/main/java/org/example/capstonedesign1/domain/cardproduct/dto/response/CardProductRecommendationResponse.java
@@ -1,18 +1,21 @@
 package org.example.capstonedesign1.domain.cardproduct.dto.response;
 
-import org.example.capstonedesign1.domain.cardproduct.entity.CardProductRecommendation;
-
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record CardProductRecommendationResponse(UUID id,
-                                                String strategy,
-                                                String content,
-                                                LocalDateTime createdAt) {
+import org.example.capstonedesign1.domain.cardproduct.entity.CardProductRecommendation;
 
-    public static CardProductRecommendationResponse from(CardProductRecommendation recommendation) {
-        return new CardProductRecommendationResponse(
-                recommendation.getId(), recommendation.getStrategy(), recommendation.getContent(), recommendation.getCreatedAt());
-    }
+public record CardProductRecommendationResponse(UUID id,
+												String strategy,
+												String content,
+												LocalDateTime createdAt) {
+
+	public static CardProductRecommendationResponse from(CardProductRecommendation recommendation) {
+		return new CardProductRecommendationResponse(
+			recommendation.getId(),
+			recommendation.getStrategy(),
+			recommendation.getContent(),
+			recommendation.getCreatedAt());
+	}
 
 }
--- src/main/java/org/example/capstonedesign1/domain/cardproduct/service/CardProductQueryService.java
+++ src/main/java/org/example/capstonedesign1/domain/cardproduct/service/CardProductQueryService.java
@@ -1,6 +1,8 @@
 package org.example.capstonedesign1.domain.cardproduct.service;
 
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+import java.util.UUID;
+
 import org.example.capstonedesign1.domain.cardproduct.dto.response.CardProductRecommendationResponse;
 import org.example.capstonedesign1.domain.cardproduct.dto.response.projection.CardProductRecommendationPreview;
 import org.example.capstonedesign1.domain.cardproduct.entity.CardProduct;
@@ -19,38 +21,38 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.UUID;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class CardProductQueryService {
-    private final CardProductRepository cardProductRepository;
-    private final CardProductRecommendationRepository cardProductRecommendationRepository;
-    private final UserQueryService userQueryService;
+	private final UserQueryService userQueryService;
 
-    public CardProductRecommendation findRecommendationById(UUID recommendationId) {
-        return cardProductRecommendationRepository.findById(recommendationId).orElseThrow(()
-                -> new NotFoundException(ErrorCode.CARD_PRODUCT_RECOMMENDATION_NOT_FOUND));
-    }
+	private final CardProductRepository cardProductRepository;
+	private final CardProductRecommendationRepository cardProductRecommendationRepository;
 
-    public PaginationResponse<CardProductRecommendationPreview> getRecommendations(User user, int page, int size) {
-        Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
-        Page<CardProductRecommendationPreview> cardProductRecommendations =
-                cardProductRecommendationRepository.findCardProductRecommendationsByUser(user, pageable);
-        return PaginationResponse.from(cardProductRecommendations);
-    }
+	public CardProductRecommendation findRecommendationById(UUID recommendationId) {
+		return cardProductRecommendationRepository.findById(recommendationId).orElseThrow(()
+			-> new NotFoundException(ErrorCode.CARD_PRODUCT_RECOMMENDATION_NOT_FOUND));
+	}
 
-    public CardProductRecommendationResponse getRecommendation(User user, UUID recommendationId) {
-        CardProductRecommendation recommendation = findRecommendationById(recommendationId);
-        if (!userQueryService.isSameUser(user, recommendation.getUser())) {
-            throw new ForbiddenException(ErrorCode.UN_AUTHORIZED);
-        }
-        return CardProductRecommendationResponse.from(recommendation);
-    }
+	public PaginationResponse<CardProductRecommendationPreview> getRecommendations(User user, int page, int size) {
+		Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
+		Page<CardProductRecommendationPreview> cardProductRecommendations =
+			cardProductRecommendationRepository.findCardProductRecommendationsByUser(user, pageable);
+		return PaginationResponse.from(cardProductRecommendations);
+	}
 
-    public List<CardProduct> findByIds(List<UUID> cardProductIds) {
-        return cardProductRepository.findAllById(cardProductIds);
-    }
+	public CardProductRecommendationResponse getRecommendation(User user, UUID recommendationId) {
+		CardProductRecommendation recommendation = findRecommendationById(recommendationId);
+		if (!userQueryService.isSameUser(user, recommendation.getUser())) {
+			throw new ForbiddenException(ErrorCode.UN_AUTHORIZED);
+		}
+		return CardProductRecommendationResponse.from(recommendation);
+	}
+
+	public List<CardProduct> findByIds(List<UUID> cardProductIds) {
+		return cardProductRepository.findAllById(cardProductIds);
+	}
 
 }
