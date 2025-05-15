2ff76ae4bd16d428d231a0ea424db59d19e263f4
jher235 <tim668666@gmail.com>
2025-05-15 19:15:05 +0900

    style: 카멜케이스 오타 수정 - #26

--- src/main/java/org/example/capstonedesign1/domain/bankproduct/service/BankProductCommandService.java
+++ src/main/java/org/example/capstonedesign1/domain/bankproduct/service/BankProductCommandService.java
@@ -1,7 +1,9 @@
 package org.example.capstonedesign1.domain.bankproduct.service;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import static org.example.capstonedesign1.global.openai.client.OpenAiApiClient.*;
+
+import java.util.List;
+
 import org.example.capstonedesign1.domain.bankproduct.dto.json.BankProductRecommendationContent;
 import org.example.capstonedesign1.domain.bankproduct.dto.request.BankProductRecommendRequest;
 import org.example.capstonedesign1.domain.bankproduct.dto.response.BankProductRecommendationResponse;
@@ -18,49 +20,48 @@ import org.example.capstonedesign1.global.openai.template.PromptTemplate;
 import org.example.capstonedesign1.global.util.JsonUtil;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
-import static org.example.capstonedesign1.global.openai.client.OpenAiApiClient.SYSTEM_ROLE;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class BankProductCommandService {
-    private final UserQueryService userQueryService;
+	private final UserQueryService userQueryService;
 
-    private final BankProductRepository bankProductRepository;
-    private final BankProductRecommendationRepository bankProductRecommendationRepository;
+	private final BankProductRepository bankProductRepository;
+	private final BankProductRecommendationRepository bankProductRecommendationRepository;
 
-    private final OpenAiApiClient openAiApiClient;
+	private final OpenAiApiClient openAiApiClient;
 
-    /**
-     * 유저의 성향, 추천 요구사항에 맞는 상품 목록들을 DB에서 가져온 후 유저 정보와 금융 상품 목록을 바탕으로 금융 상품 추천 진행
-     * strategy 는 정규화하지만, 추천 받은 상품 목록은 json으로 저장함. json으로 저장 시 칼럼명이 함께 저장되고 json 형태를 유지하므로 사이즈가 늘어난다는 단점이 존재하나
-     * 과도한 정규화로 인해 조회 시 join 연산을 불필요하게 수행하는 것을 방지하기 위해 json으로 저장함
-     *
-     * @param user
-     * @param request
-     * @return
-     */
-    public BankProductRecommendationResponse recommendBankProduct(User user, BankProductRecommendRequest request) {
-//        Propensity propensity = userQueryService.ensureUserPropensity(user); // 현재는 요구사항 상 유저의 성향을 선택하지 않으므로 주석 처리 됨
-        Propensity propensity = request.getPropensity();
+	/**
+	 * 유저의 성향, 추천 요구사항에 맞는 상품 목록들을 DB에서 가져온 후 유저 정보와 금융 상품 목록을 바탕으로 금융 상품 추천 진행
+	 * strategy 는 정규화하지만, 추천 받은 상품 목록은 json으로 저장함. json으로 저장 시 칼럼명이 함께 저장되고 json 형태를 유지하므로 사이즈가 늘어난다는 단점이 존재하나
+	 * 과도한 정규화로 인해 조회 시 join 연산을 불필요하게 수행하는 것을 방지하기 위해 json으로 저장함
+	 *
+	 * @param user
+	 * @param request
+	 * @return
+	 */
+	public BankProductRecommendationResponse recommendBankProduct(User user, BankProductRecommendRequest request) {
+		//        Propensity propensity = userQueryService.ensureUserPropensity(user); // 현재는 요구사항 상 유저의 성향을 선택하지 않으므로 주석 처리 됨
+		Propensity propensity = request.getPropensity();
 
-        List<BankProduct> bankProducts =
-                bankProductRepository.findRecommendable(propensity, request.getAmount(), request.getTerm());
+		List<BankProduct> bankProducts =
+			bankProductRepository.findRecommendable(propensity, request.getAmount(), request.getTerm());
 
-        String requestMessage = PromptTemplate.BankProductRecommendPrompt(user, request, bankProducts);
-        String content = openAiApiClient.sendRequest(List.of(new Message(SYSTEM_ROLE, requestMessage)));
+		String requestMessage = PromptTemplate.bankProductRecommendPrompt(user, request, bankProducts);
+		String content = openAiApiClient.sendRequest(List.of(new Message(SYSTEM_ROLE, requestMessage)));
 
-        BankProductRecommendationContent bankProductRecommendationContent =
-                JsonUtil.parseClass(BankProductRecommendationContent.class, content);
+		BankProductRecommendationContent bankProductRecommendationContent =
+			JsonUtil.parseClass(BankProductRecommendationContent.class, content);
 
-        String recommendationJson = JsonUtil.convertToJson(bankProductRecommendationContent.recommendations());
+		String recommendationJson = JsonUtil.convertToJson(bankProductRecommendationContent.recommendations());
 
-        BankProductRecommendation recommendation = new BankProductRecommendation(
-                user, bankProductRecommendationContent.strategy(), recommendationJson);
-        bankProductRecommendationRepository.save(recommendation);
-        return BankProductRecommendationResponse.from(recommendation);
-    }
+		BankProductRecommendation recommendation = new BankProductRecommendation(
+			user, bankProductRecommendationContent.strategy(), recommendationJson);
+		bankProductRecommendationRepository.save(recommendation);
+		return BankProductRecommendationResponse.from(recommendation);
+	}
 
 }
--- src/main/java/org/example/capstonedesign1/global/openai/template/PromptTemplate.java
+++ src/main/java/org/example/capstonedesign1/global/openai/template/PromptTemplate.java
@@ -1,5 +1,10 @@
 package org.example.capstonedesign1.global.openai.template;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+import java.util.Optional;
+
 import org.example.capstonedesign1.domain.bankproduct.dto.request.BankProductRecommendRequest;
 import org.example.capstonedesign1.domain.bankproduct.entity.BankProduct;
 import org.example.capstonedesign1.domain.cardproduct.entity.CardProduct;
@@ -7,233 +12,225 @@ import org.example.capstonedesign1.domain.propensity.dto.request.SurveyRequest;
 import org.example.capstonedesign1.domain.user.entity.Profile;
 import org.example.capstonedesign1.domain.user.entity.User;
 
-import java.time.LocalDate;
-import java.time.Period;
-import java.util.List;
-import java.util.Optional;
-
-
 public class PromptTemplate {
-    private static final String BASE_TEMPLATE = """
-            ### 요청하고 싶은 것
-            {{request}}
-                    
-            ### 응답 값 형식
-            {{responseFormat}}
-            """;
+	private static final String BASE_TEMPLATE = """
+		### 요청하고 싶은 것
+		{{request}}
+		        
+		### 응답 값 형식
+		{{responseFormat}}
+		""";
 
-    private static final String SURVEY_ENTRY_TEMPLATE = """
-            ### 질문: %s
-            ### 응답: %s
-                        
-            """;
+	private static final String SURVEY_ENTRY_TEMPLATE = """
+		### 질문: %s
+		### 응답: %s
+		            
+		""";
 
-    public static String conversationPrompt(String requestMessage,
-                                            Optional<String> summary,
-                                            List<BankProduct> bankProducts,
-                                            List<CardProduct> cardProducts) {
+	public static String conversationPrompt(String requestMessage,
+		Optional<String> summary,
+		List<BankProduct> bankProducts,
+		List<CardProduct> cardProducts) {
 
-        StringBuilder stringBuilder = new StringBuilder();
-        bankProducts.stream()
-                .forEach(product -> stringBuilder.append(product.toString()).append("\n\n"));
-        cardProducts.stream()
-                .forEach(product -> stringBuilder.append(product.toString()).append("\n\n"));
+		StringBuilder stringBuilder = new StringBuilder();
+		bankProducts.stream()
+			.forEach(product -> stringBuilder.append(product.toString()).append("\n\n"));
+		cardProducts.stream()
+			.forEach(product -> stringBuilder.append(product.toString()).append("\n\n"));
 
+		return fillTemplate(
+			"""
+				## 명령
+				금융 관련 대화를 진행하는 입장이야
+				결과는 JSON 형식으로 반환해줘.
+				                        
+				## 사용자의 input
+				%s
+				                        
+				## 이전 대화 요약
+				%s
+				                        
+				## 관련 상품 목록
+				%s
+				                        
+				                        
+				""".formatted(requestMessage, summary.orElse("현재는 첫 대화로, 요약이 없음"), stringBuilder),
+			"""
+				{
+				    "message": "<응답 메세지>",
+				    "summary": "<이전 내용 + 현재 대화 내용을 모두 합친 요약>"           
+				}
+				"""
+		);
+	}
 
-        return fillTemplate(
-                """
-                        ## 명령
-                        금융 관련 대화를 진행하는 입장이야
-                        결과는 JSON 형식으로 반환해줘.
-                                                
-                        ## 사용자의 input
-                        %s
-                                                
-                        ## 이전 대화 요약
-                        %s
-                                                
-                        ## 관련 상품 목록
-                        %s
-                                                
-                                                
-                        """.formatted(requestMessage, summary.orElse("현재는 첫 대화로, 요약이 없음"), stringBuilder),
-                """
-                        {
-                            "message": "<응답 메세지>",
-                            "summary": "<이전 내용 + 현재 대화 내용을 모두 합친 요약>"           
-                        }
-                        """
-        );
-    }
+	public static String fillTemplate(String request, String responseFormat) {
+		return BASE_TEMPLATE.replace("{{request}}", request)
+			.replace("{{responseFormat}}", responseFormat);
+	}
 
+	//이걸 스트링 빌더로 묶어서 처리
+	public static String entry(String question, String answer) {
+		return String.format(SURVEY_ENTRY_TEMPLATE, question, answer);
+	}
 
-    public static String fillTemplate(String request, String responseFormat) {
-        return BASE_TEMPLATE.replace("{{request}}", request)
-                .replace("{{responseFormat}}", responseFormat);
-    }
+	public static String propensityAnalysisPrompt(String gender, String analyzedPoint) {
+		return fillTemplate(
+			"""
+				## 명령
+				성별과 사용자의 설문 결과로 추출한 성향 점수를 기반으로 금융 성향 타입
+				(보수형(저축과 안정 추구), 소비형(즉각적 소비 선호), 투자형(위험 감수 및 수익 추구), 균형형(저축과 소비의 조화)**, 융합형(상황에 따라 유동적으로 반응)**) 을 분석하고, 
+				타입에 대한 설명, 장단점, 주의점을 기계적이지 않고 자세하게 답변해줘.
+				결과는 JSON 형식으로 반환해줘.
 
-    //이걸 스트링 빌더로 묶어서 처리
-    public static String entry(String question, String answer) {
-        return String.format(SURVEY_ENTRY_TEMPLATE, question, answer);
-    }
+				## 사용자 정보
+				성별: %s
 
-    public static String propensityAnalysisPrompt(String gender, String analyzedPoint) {
-        return fillTemplate(
-                """
-                        ## 명령
-                        성별과 사용자의 설문 결과로 추출한 성향 점수를 기반으로 금융 성향 타입
-                        (보수형(저축과 안정 추구), 소비형(즉각적 소비 선호), 투자형(위험 감수 및 수익 추구), 균형형(저축과 소비의 조화)**, 융합형(상황에 따라 유동적으로 반응)**) 을 분석하고, 
-                        타입에 대한 설명, 장단점, 주의점을 기계적이지 않고 자세하게 답변해줘.
-                        결과는 JSON 형식으로 반환해줘.
+				## 사용자의 성향 점수
+				%s
+				""".formatted(gender, analyzedPoint),
+			"""
+				{
+				    "type": "<금융 성향 유형>",
+				    "description": "<금융 성향 설명>",
+				    "prosAndCons": "<금융 성향 장단점 설명>",
+				    "precaution": "<해당 금융 성향인들 주의점>"
+				}
+				"""
+		);
+	}
 
-                        ## 사용자 정보
-                        성별: %s
+	public static String propensityAnalysisPrompt(String gender, List<SurveyRequest.SurveyItemEntry> surveyEntries) {
+		StringBuilder stringBuilder = new StringBuilder();
+		surveyEntries.stream()
+			.forEach(e -> stringBuilder.append(entry(e.getQuestion(), e.getSelectedAnswer())));
 
-                        ## 사용자의 성향 점수
-                        %s
-                        """.formatted(gender, analyzedPoint),
-                """
-                        {
-                            "type": "<금융 성향 유형>",
-                            "description": "<금융 성향 설명>",
-                            "prosAndCons": "<금융 성향 장단점 설명>",
-                            "precaution": "<해당 금융 성향인들 주의점>"
-                        }
-                        """
-        );
-    }
+		return fillTemplate(
+			"""
+				## 명령
+				성별과 사용자의 답변을 기반으로 금융 성향 타입
+				(보수형(저축과 안정 추구), 소비형(즉각적 소비 선호), 투자형(위험 감수 및 수익 추구), 균형형(저축과 소비의 조화)**, 융합형(상황에 따라 유동적으로 반응)**) 을 분석하고, 
+				타입에 대한 설명, 장단점, 주의점을 기계적이지 않고 자세하게 답변해줘.
+				결과는 JSON 형식으로 반환해줘.
 
-    public static String propensityAnalysisPrompt(String gender, List<SurveyRequest.SurveyItemEntry> surveyEntries) {
-        StringBuilder stringBuilder = new StringBuilder();
-        surveyEntries.stream()
-                .forEach(e -> stringBuilder.append(entry(e.getQuestion(), e.getSelectedAnswer())));
+				## 사용자 정보
+				성별: %s
 
-        return fillTemplate(
-                """
-                        ## 명령
-                        성별과 사용자의 답변을 기반으로 금융 성향 타입
-                        (보수형(저축과 안정 추구), 소비형(즉각적 소비 선호), 투자형(위험 감수 및 수익 추구), 균형형(저축과 소비의 조화)**, 융합형(상황에 따라 유동적으로 반응)**) 을 분석하고, 
-                        타입에 대한 설명, 장단점, 주의점을 기계적이지 않고 자세하게 답변해줘.
-                        결과는 JSON 형식으로 반환해줘.
+				## 답변
+				%s
+				""".formatted(gender, stringBuilder),
+			"""
+				{
+				    "type": "<금융 성향 유형>",
+				    "description": "<금융 성향 설명>",
+				    "prosAndCons": "<금융 성향 장단점 설명>",
+				    "precaution": "<해당 금융 성향인들 주의점>"
+				}
+				"""
+		);
+	}
 
-                        ## 사용자 정보
-                        성별: %s
+	public static String bankProductRecommendPrompt(User user, BankProductRecommendRequest request,
+		List<BankProduct> products) {
+		Profile profile = user.getProfile();
+		StringBuilder stringBuilder = new StringBuilder();
+		products.stream()
+			.forEach(product -> stringBuilder.append(product.toString()).append("\n\n"));
+		return fillTemplate(
+			"""
+				## 명령
+				사용자 정보와 사용자의 요구사항을 기반으로 금융 상품을 추천해줘.
+				금융 상품 목록은 DB 내의 사용자 금융 성향과 일치하는 상품들을 분류해놓은 거야.
+				결과는 JSON 형식으로 반환해줘. 결과에서 recommendations 목록은 1 ~ 3개까지 넣어줘
 
-                        ## 답변
-                        %s
-                        """.formatted(gender, stringBuilder),
-                """
-                        {
-                            "type": "<금융 성향 유형>",
-                            "description": "<금융 성향 설명>",
-                            "prosAndCons": "<금융 성향 장단점 설명>",
-                            "precaution": "<해당 금융 성향인들 주의점>"
-                        }
-                        """
-        );
-    }
+				## 사용자 정보
+				금융 성향: %s
+				성별: %s
+				만 나이: %s
+				월 수입: %s
+				자산: %s
+				                
+				## 사용자 요구
+				투자 금액: %s 원
+				투자 기간: %s 개월
+				                
+				## 금융 상품 목록
+				%s
 
-    public static String BankProductRecommendPrompt(User user, BankProductRecommendRequest request, List<BankProduct> products) {
-        Profile profile = user.getProfile();
-        StringBuilder stringBuilder = new StringBuilder();
-        products.stream()
-                .forEach(product -> stringBuilder.append(product.toString()).append("\n\n"));
-        return fillTemplate(
-                """
-                        ## 명령
-                        사용자 정보와 사용자의 요구사항을 기반으로 금융 상품을 추천해줘.
-                        금융 상품 목록은 DB 내의 사용자 금융 성향과 일치하는 상품들을 분류해놓은 거야.
-                        결과는 JSON 형식으로 반환해줘. 결과에서 recommendations 목록은 1 ~ 3개까지 넣어줘
+				""".formatted(
+				profile.getPropensity().getName(),
+				profile.getGender(),
+				Period.between(profile.getBirthDate(), LocalDate.now()),
+				profile.getSalary(),
+				profile.getAsset(),
+				request.getAmount(),
+				request.getTerm(),
+				stringBuilder
+			),
+			"""
+				{
+				  "recommendations": [
+				    {
+				      "id": "<금융 상품 ID>",
+				      "description": "<금융 상품 설명>",
+				      "reason": "<추천 이유 및 장단점>",
+				      "detailUrl": "<상세 URL>"
+				    }
+				  ],
+				  "strategy": "<금융 상품 추천 이유 및 전략 설명>"
+				}
+				"""
+		);
+	}
 
-                        ## 사용자 정보
-                        금융 성향: %s
-                        성별: %s
-                        만 나이: %s
-                        월 수입: %s
-                        자산: %s
-                                        
-                        ## 사용자 요구
-                        투자 금액: %s 원
-                        투자 기간: %s 개월
-                                        
-                        ## 금융 상품 목록
-                        %s
+	public static String CardProductRecommendPrompt(User user, String paymentRecord, List<CardProduct> products) {
+		Profile profile = user.getProfile();
+		StringBuilder stringBuilder = new StringBuilder();
+		products.stream()
+			.forEach(product -> stringBuilder.append(product.toString()).append("\n\n"));
+		return fillTemplate(
+			"""
+				## 명령
+				사용자의 최근 결제 내역을 중심으로, 금융 성향 + 정보를 고려하여 카드 상품을 추천해줘.
+				카드 상품 목록은 DB 내의 사용자 금융 성향과 일치하는 상품들을 분류해놓은 거야.
+				결과는 JSON 형식으로 반환해줘. 결과에서 recommendations 목록은 1 ~ 3개까지 넣어줘
 
-                        """.formatted(
-                        profile.getPropensity().getName(),
-                        profile.getGender(),
-                        Period.between(profile.getBirthDate(), LocalDate.now()),
-                        profile.getSalary(),
-                        profile.getAsset(),
-                        request.getAmount(),
-                        request.getTerm(),
-                        stringBuilder
-                ),
-                """
-                        {
-                          "recommendations": [
-                            {
-                              "id": "<금융 상품 ID>",
-                              "description": "<금융 상품 설명>",
-                              "reason": "<추천 이유 및 장단점>",
-                              "detailUrl": "<상세 URL>"
-                            }
-                          ],
-                          "strategy": "<금융 상품 추천 이유 및 전략 설명>"
-                        }
-                        """
-        );
-    }
+				## 사용자 정보
+				금융 성향: %s
+				성별: %s
+				만 나이: %s
+				월 수입: %s
+				자산: %s
+				                
+				## 사용자의 최근 결제 내역
+				%s
+				                
+				## 카드 상품 목록
+				%s
 
-    public static String CardProductRecommendPrompt(User user, String paymentRecord, List<CardProduct> products) {
-        Profile profile = user.getProfile();
-        StringBuilder stringBuilder = new StringBuilder();
-        products.stream()
-                .forEach(product -> stringBuilder.append(product.toString()).append("\n\n"));
-        return fillTemplate(
-                """
-                        ## 명령
-                        사용자의 최근 결제 내역을 중심으로, 금융 성향 + 정보를 고려하여 카드 상품을 추천해줘.
-                        카드 상품 목록은 DB 내의 사용자 금융 성향과 일치하는 상품들을 분류해놓은 거야.
-                        결과는 JSON 형식으로 반환해줘. 결과에서 recommendations 목록은 1 ~ 3개까지 넣어줘
-
-                        ## 사용자 정보
-                        금융 성향: %s
-                        성별: %s
-                        만 나이: %s
-                        월 수입: %s
-                        자산: %s
-                                        
-                        ## 사용자의 최근 결제 내역
-                        %s
-                                        
-                        ## 카드 상품 목록
-                        %s
-
-                        """.formatted(
-                        profile.getPropensity().getName(),
-                        profile.getGender(),
-                        Period.between(profile.getBirthDate(), LocalDate.now()),
-                        profile.getSalary(),
-                        profile.getAsset(),
-                        paymentRecord,
-                        stringBuilder
-                ),
-                """
-                        {
-                          "recommendations": [
-                            {
-                              "id": "<카드 상품 ID>",
-                              "description": "<카드 상품 설명>",
-                              "reason": "<추천 이유 및 장단점>",
-                              "detailUrl": "<상세 URL>"
-                            }
-                          ],
-                          "strategy": "<종합적인 상품 추천 이유 및 전략 설명>"
-                        }
-                        """
-        );
-    }
-
+				""".formatted(
+				profile.getPropensity().getName(),
+				profile.getGender(),
+				Period.between(profile.getBirthDate(), LocalDate.now()),
+				profile.getSalary(),
+				profile.getAsset(),
+				paymentRecord,
+				stringBuilder
+			),
+			"""
+				{
+				  "recommendations": [
+				    {
+				      "id": "<카드 상품 ID>",
+				      "description": "<카드 상품 설명>",
+				      "reason": "<추천 이유 및 장단점>",
+				      "detailUrl": "<상세 URL>"
+				    }
+				  ],
+				  "strategy": "<종합적인 상품 추천 이유 및 전략 설명>"
+				}
+				"""
+		);
+	}
 
 }
