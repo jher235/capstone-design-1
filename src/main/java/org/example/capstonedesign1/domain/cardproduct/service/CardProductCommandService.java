6d7ae6f59208eb56384468465cba2e630b7873ba
jher235 <tim668666@gmail.com>
2025-05-16 00:28:38 +0900

    feat: 카드 추천 받기 로직 수정 - #26 - 기존에는 카드 성향과 유저 성향을 매칭하는 방식이었는데, 요구사항에 따라 이를 수정함. - 수정된 형태는 카드에는 카테고리가 존재하고 유저의 카드 소비 내역을 분석하여 소비 내역이 많은 카테고리 + 소비 내역 요약을 추출함. 이후 해당 카테고리에 대한 혜택을 많이 받을 수 있는 카드들을 리스트업 하여 llm에게 카드 소비 내역 요약본과 카드 상품 목록들을 전달하여 사용자에게 맞는 카드 상품을 추출함.

--- src/main/java/org/example/capstonedesign1/domain/cardproduct/service/CardProductCommandService.java
+++ src/main/java/org/example/capstonedesign1/domain/cardproduct/service/CardProductCommandService.java
@@ -1,7 +1,11 @@
 package org.example.capstonedesign1.domain.cardproduct.service;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.GeneralSecurityException;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.apache.poi.poifs.crypt.Decryptor;
 import org.apache.poi.poifs.crypt.EncryptionInfo;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
@@ -11,14 +15,14 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.example.capstonedesign1.domain.cardproduct.dto.json.CardProductRecommendationContent;
+import org.example.capstonedesign1.domain.cardproduct.dto.json.ConsumptionAnalysis;
 import org.example.capstonedesign1.domain.cardproduct.dto.response.CardProductRecommendationResponse;
 import org.example.capstonedesign1.domain.cardproduct.entity.CardProduct;
 import org.example.capstonedesign1.domain.cardproduct.entity.CardProductRecommendation;
+import org.example.capstonedesign1.domain.cardproduct.entity.Category;
 import org.example.capstonedesign1.domain.cardproduct.repository.CardProductRecommendationRepository;
 import org.example.capstonedesign1.domain.cardproduct.repository.CardProductRepository;
-import org.example.capstonedesign1.domain.propensity.entity.enums.Propensity;
 import org.example.capstonedesign1.domain.user.entity.User;
-import org.example.capstonedesign1.domain.user.service.UserQueryService;
 import org.example.capstonedesign1.global.dto.response.Message;
 import org.example.capstonedesign1.global.exception.BadRequestException;
 import org.example.capstonedesign1.global.exception.ForbiddenException;
@@ -29,128 +33,161 @@ import org.example.capstonedesign1.global.util.JsonUtil;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.security.GeneralSecurityException;
-import java.util.List;
-
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Service
 @RequiredArgsConstructor
 public class CardProductCommandService {
-    private static final int MAX_ROW = 100;
-    private static final int START_ROW = 9;
-    private static final String DELIMITER = ", ";
+	private static final int MAX_ROW = 100;
+	private static final int START_ROW = 9;
+	private static final String DELIMITER = ", ";
+	private static final int MAX_RECOMMENDABLE_CARD_PRODUCT_COUNT = 10;
 
-    private final UserQueryService userQueryService;
+	private final CategoryQueryService categoryQueryService;
 
-    private final CardProductRepository cardProductRepository;
-    private final CardProductRecommendationRepository cardProductRecommendationRepository;
+	private final CardProductRepository cardProductRepository;
+	private final CardProductRecommendationRepository cardProductRecommendationRepository;
 
-    private final OpenAiApiClient openAiApiClient;
+	private final OpenAiApiClient openAiApiClient;
 
-    public CardProductRecommendationResponse recommendCardProduct(User user, MultipartFile file) {
-        validXlsxFile(file);
-        String paymentRecord = resolvePaymentRecord(user, file); //비동기로 처리하면 더 빠를 것 같음.
+	public CardProductRecommendationResponse recommendCardProduct(User user, MultipartFile file) {
+		List<Category> categories = categoryQueryService.getAllCategory();
 
-        Propensity userPropensity = userQueryService.ensureUserPropensity(user);
-        List<CardProduct> cardProducts = cardProductRepository.getRecommendable(userPropensity);
+		ConsumptionAnalysis consumptionAnalysis = getConsumeAnalysis(user, file, categories);
 
-        String prompt = PromptTemplate.CardProductRecommendPrompt(user, paymentRecord, cardProducts);
-        String response = openAiApiClient.sendRequest(List.of(new Message(OpenAiApiClient.SYSTEM_ROLE, prompt)));
+		List<Category> categoryList = getConsumedCategory(categories, consumptionAnalysis.categoryNames());
+		List<CardProduct> recommendableCardProducts = cardProductRepository.findByCategoryList(categoryList);
 
-        CardProductRecommendationContent content = JsonUtil.parseClass(CardProductRecommendationContent.class, response);
+		CardProductRecommendationContent content =
+			recommendFromProductsAndAnalysis(consumptionAnalysis.analysis(), recommendableCardProducts);
+		String recommendationJson = JsonUtil.convertToJson(content.recommendations());
 
-        String recommendationJson = JsonUtil.convertToJson(content.recommendations());
+		CardProductRecommendation cardProductRecommendation = CardProductRecommendation.builder()
+			.user(user)
+			.consumptionAnalysis(consumptionAnalysis.analysis())
+			.strategy(content.strategy())
+			.content(recommendationJson)
+			.build();
 
-        CardProductRecommendation cardProductRecommendation = new CardProductRecommendation(
-                user, content.strategy(), recommendationJson);
-        cardProductRecommendationRepository.save(cardProductRecommendation);
-        return CardProductRecommendationResponse.from(cardProductRecommendation);
-    }
+		CardProductRecommendation savedRecommendation = cardProductRecommendationRepository.save(
+			cardProductRecommendation);
+		return CardProductRecommendationResponse.from(savedRecommendation);
+	}
 
-    private void validXlsxFile(MultipartFile file) {
-        if (file.isEmpty()
-                || !file.getOriginalFilename().toLowerCase().endsWith(".xlsx")) {
-            throw new BadRequestException(ErrorCode.INVALID_FILE);
-        }
-    }
+	private CardProductRecommendationContent recommendFromProductsAndAnalysis(
+		String consumptionAnalysis, List<CardProduct> cardProducts) {
+		List<CardProduct> recommendableCardProducts =
+			cardProducts.subList(0, Math.min(cardProducts.size(), MAX_RECOMMENDABLE_CARD_PRODUCT_COUNT));
 
+		String prompt = PromptTemplate.cardProductRecommendPrompt(consumptionAnalysis, recommendableCardProducts);
+		String response = openAiApiClient.sendRequest(List.of(new Message(OpenAiApiClient.SYSTEM_ROLE, prompt)));
 
-    private String resolvePaymentRecord(User user, MultipartFile file) {
-        //try with resource
-        try (InputStream inputStream = file.getInputStream()) {
-            POIFSFileSystem fs = new POIFSFileSystem(inputStream);
-            EncryptionInfo info = new EncryptionInfo(fs);
-            Decryptor decryptor = Decryptor.getInstance(info);
+		return JsonUtil.parseClass(CardProductRecommendationContent.class, response);
+	}
 
-            if (decryptor.verifyPassword(user.get6DigitBirthDate())) {
-                try (InputStream decryptedStream = decryptor.getDataStream(fs);
-                     Workbook workbook = new XSSFWorkbook(decryptedStream)) {
-                    Sheet sheet = workbook.getSheetAt(0); // 첫 번째 시트 읽기
-                    return paymentRecordFormat(sheet);
-                }
-            }
-            throw new ForbiddenException(ErrorCode.PAYMENT_XLSX_UNAUTHORIZED);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (GeneralSecurityException e) {
-            throw new ForbiddenException(ErrorCode.PAYMENT_XLSX_UNAUTHORIZED, "결제 내역 파일의 PW가 일치하지 않습니다.");
-        }
-    }
+	private ConsumptionAnalysis getConsumeAnalysis(User user, MultipartFile file, List<Category> categories) {
+		validXlsxFile(file);
+		String consumptionRecord = resolveConsumptionRecord(user, file);
+		String prompt = PromptTemplate.consumeAnalysisPrompt(getAllCategoryNameToString(), consumptionRecord);
+		String response = openAiApiClient.sendRequest(List.of(new Message(OpenAiApiClient.SYSTEM_ROLE, prompt)));
 
-    private String paymentRecordFormat(Sheet sheet) {
-        StringBuilder sb = new StringBuilder();
+		return JsonUtil.parseClass(ConsumptionAnalysis.class, response);
+	}
 
-        int maxRow = Math.min(MAX_ROW, sheet.getLastRowNum());
-        for (int i = START_ROW; i < maxRow; i++) { // START_ROW 이전에는 필요없는 정보들이므로 제외
-            Row row = sheet.getRow(i);
-            if (row == null) {
-                continue;
-            }
-            for (int j = 0; j < row.getLastCellNum() - 1; j++) { // 거래 후 잔액은 제외하기 위해서 -1을 함
-                Cell cell = row.getCell(j);
-                if (cell == null) {
-                    continue;
-                } else if (j == 1) {
-                    sb.append(parseTFromDTColumn(cell.getStringCellValue()));
-                } else {
-                    sb.append(getValueFromCell(cell));
-                }
-                sb.append(DELIMITER);
-            }
-            sb.append("\n");
-        }
+	private List<Category> getConsumedCategory(List<Category> allCategories, List<String> categoryNames) {
+		return allCategories.stream()
+			.filter(category -> categoryNames.contains(category.getName()))
+			.toList();
+	}
 
-        return sb.toString();
-    }
+	private String getAllCategoryNameToString() {
+		List<Category> categories = categoryQueryService.getAllCategory();
+		return categories.stream()
+			.map(Category::getName)
+			.collect(Collectors.joining(DELIMITER));
+	}
 
-    /**
-     * DateTime 에서 날짜, 시간만 추출하여 반환. 년도, 초 등은 제외
-     *
-     * @param cell
-     * @return
-     */
-    private String parseTFromDTColumn(String cell) {
-        if (cell.length() > 16) {
-            return cell.substring(5, 16);
-        }
-        return cell;
-    }
+	private void validXlsxFile(MultipartFile file) {
+		if (file.isEmpty()
+			|| !file.getOriginalFilename().toLowerCase().endsWith(".xlsx")) {
+			throw new BadRequestException(ErrorCode.INVALID_FILE);
+		}
+	}
 
-    private String getValueFromCell(Cell cell) {
-        switch (cell.getCellType()) {
-            case STRING:
-                return cell.getStringCellValue();
-            case NUMERIC:
-                return String.valueOf((long) cell.getNumericCellValue());
-            case BOOLEAN:
-                return String.valueOf(cell.getBooleanCellValue());
-            default:
-                return "N/A";
-        }
-    }
+	private String resolveConsumptionRecord(User user, MultipartFile file) {
+		//try with resource
+		try (InputStream inputStream = file.getInputStream()) {
+			POIFSFileSystem fs = new POIFSFileSystem(inputStream);
+			EncryptionInfo info = new EncryptionInfo(fs);
+			Decryptor decryptor = Decryptor.getInstance(info);
 
+			if (decryptor.verifyPassword(user.get6DigitBirthDate())) {
+				try (InputStream decryptedStream = decryptor.getDataStream(fs);
+					 Workbook workbook = new XSSFWorkbook(decryptedStream)) {
+					Sheet sheet = workbook.getSheetAt(0); // 첫 번째 시트 읽기
+					return paymentRecordFormat(sheet);
+				}
+			}
+			throw new ForbiddenException(ErrorCode.PAYMENT_XLSX_UNAUTHORIZED);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} catch (GeneralSecurityException e) {
+			throw new ForbiddenException(ErrorCode.PAYMENT_XLSX_UNAUTHORIZED, "결제 내역 파일의 PW가 일치하지 않습니다.");
+		}
+	}
+
+	private String paymentRecordFormat(Sheet sheet) {
+		StringBuilder sb = new StringBuilder();
+
+		int maxRow = Math.min(MAX_ROW, sheet.getLastRowNum());
+		for (int i = START_ROW; i < maxRow; i++) { // START_ROW 이전에는 필요없는 정보들이므로 제외
+			Row row = sheet.getRow(i);
+			if (row == null) {
+				continue;
+			}
+			for (int j = 0; j < row.getLastCellNum() - 1; j++) { // 거래 후 잔액은 제외하기 위해서 -1을 함
+				Cell cell = row.getCell(j);
+				if (cell == null) {
+					continue;
+				} else if (j == 1) {
+					sb.append(parseTFromDTColumn(cell.getStringCellValue()));
+				} else {
+					sb.append(getValueFromCell(cell));
+				}
+				sb.append(DELIMITER);
+			}
+			sb.append("\n");
+		}
+
+		return sb.toString();
+	}
+
+	/**
+	 * DateTime 에서 날짜, 시간만 추출하여 반환. 년도, 초 등은 제외
+	 *
+	 * @param cell
+	 * @return
+	 */
+	private String parseTFromDTColumn(String cell) {
+		if (cell.length() > 16) {
+			return cell.substring(5, 16);
+		}
+		return cell;
+	}
+
+	private String getValueFromCell(Cell cell) {
+		switch (cell.getCellType()) {
+			case STRING:
+				return cell.getStringCellValue();
+			case NUMERIC:
+				return String.valueOf((long)cell.getNumericCellValue());
+			case BOOLEAN:
+				return String.valueOf(cell.getBooleanCellValue());
+			default:
+				return "N/A";
+		}
+	}
 
 }
