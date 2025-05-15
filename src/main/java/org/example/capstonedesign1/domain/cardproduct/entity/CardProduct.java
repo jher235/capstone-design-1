f54a1e5a34ffefe0ef1092729d003b63d0c12913
jher235 <tim668666@gmail.com>
2025-05-15 23:59:39 +0900

    feat: Category 와 n:n 연관관계 작성 - #26

--- src/main/java/org/example/capstonedesign1/domain/cardproduct/entity/CardProduct.java
+++ src/main/java/org/example/capstonedesign1/domain/cardproduct/entity/CardProduct.java
@@ -1,43 +1,50 @@
 package org.example.capstonedesign1.domain.cardproduct.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.example.capstonedesign1.domain.cardproduct.entity.enums.CardProductType;
+import org.example.capstonedesign1.global.common.BaseEntity;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
-import org.example.capstonedesign1.domain.cardproduct.entity.enums.CardProductType;
-import org.example.capstonedesign1.global.common.BaseEntity;
 
 @Entity
 @Getter
 public class CardProduct extends BaseEntity {
 
-    @Column(nullable = false)
-    private String name;
-    @Enumerated(EnumType.STRING)
-    private CardProductType cardProductType;
-    @Column(nullable = false)
-    private String bankName;
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "cardProduct")
+	List<CardProductCategory> cardProductCategories = new ArrayList<>();
+	@Column(nullable = false)
+	private String name;
+	@Enumerated(EnumType.STRING)
+	private CardProductType cardProductType;
+	@Column(nullable = false)
+	private String bankName;
+	private String description;
+	private Integer annualFee;
+	@Column(length = 1000)
+	private String benefit;
+	private String detailUrl;
 
-    private String description;
-    private Integer annualFee;
-    @Column(length = 1000)
-    private String benefit;
-    private String detailUrl;
+	@Override
+	public String toString() {
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder("CardProduct{");
-        sb.append("id=" + this.getId())
-                .append(", name=" + name)
-                .append(", type=" + cardProductType)
-                .append(", bankName=" + bankName);
+		String sb = "CardProduct{" + "id=" + this.getId()
+			+ ", name=" + name
+			+ ", type=" + cardProductType
+			+ ", bankName=" + bankName
+			+ (this.description != null ? ", description=" + description : "")
+			+ ", annualFee=" + (this.annualFee != null ? annualFee : 0)
+			+ (this.benefit != null ? ", benefit=" + benefit : "")
+			+ (this.detailUrl != null ? ", detailUrl=" + detailUrl : "");
 
-        sb.append(this.description != null ? ", description=" + description : "")
-                .append(", annualFee=").append(this.annualFee != null ? annualFee : 0)
-                .append(this.benefit != null ?  ", benefit=" + benefit : "")
-                .append(this.detailUrl != null ?  ", detailUrl=" + detailUrl : "");
-
-        return sb.toString();
-    }
+		return sb;
+	}
 }
