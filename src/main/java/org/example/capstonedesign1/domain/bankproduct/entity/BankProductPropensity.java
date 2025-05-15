6b20fd7a803b3f2a3a16795e4647f987f91835fb
jher235 <tim668666@gmail.com>
2025-05-16 00:01:02 +0900

    feat: BankProductPropensity 의 pk 타입 수정 - #26 기존 uuid에서 Long으로 수정. 노출되지 않으므로 uuid를 사용할 필요가 없다고 판단함

--- src/main/java/org/example/capstonedesign1/domain/bankproduct/entity/BankProductPropensity.java
+++ src/main/java/org/example/capstonedesign1/domain/bankproduct/entity/BankProductPropensity.java
@@ -1,20 +1,32 @@
 package org.example.capstonedesign1.domain.bankproduct.entity;
 
-import jakarta.persistence.*;
-import lombok.Getter;
 import org.example.capstonedesign1.domain.propensity.entity.enums.Propensity;
-import org.example.capstonedesign1.global.common.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
 
 @Entity
 @Getter
-public class BankProductPropensity extends BaseEntity {
+public class BankProductPropensity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private Propensity propensity;
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private Propensity propensity;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "bank_product_id", nullable = false)
-    private BankProduct bankProduct;
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "bank_product_id", nullable = false)
+	private BankProduct bankProduct;
 
 }
